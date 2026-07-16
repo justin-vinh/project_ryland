e3cba1698d948a5395043f4ccaeadbfb605a2476
Justin Vinh <jvinh21@gmail.com>
2026-07-16 12:22:02 -0400

    Updated handling of VPN drops, added run_tag

--- project_ryland/llm_utils/llm_generation_utils.py
+++ project_ryland/llm_utils/llm_generation_utils.py
@@ -948,6 +948,7 @@ class LLM_wrapper:
                     "getaddrinfo failed",
                     "nodename nor servname",
                     "403",
+                    "cannot unpack non-iterable NoneType object"
                 )
                 is_fatal_connection = (
                         isinstance(
